f6cceb43b0cbde662f0d3a3bbf7cbf84c7af4c43
vvagner.silva <vvagner.silva@923c45c9-312b-0410-8b6e-5d8b477a1069>
2007-03-09 16:34:43 +0000

    Adicao dos arquivos

---
+++ trunk/Padroes de Projetos/strategy/strategy/Quack.cpp
@@ -0,0 +1,10 @@
+#include "Quack.h"
+#include <iostream>
+
+namespace strategy
+{
+   void Quack::quack() 
+   {
+      	std::cout << "Class Quack!!!" << std::endl;	
+   }
+}
---
+++ trunk/Padroes de Projetos/strategy/strategy/QuackBehavior.h
@@ -0,0 +1,17 @@
+#ifndef _quackbehavior_
+#define _quackbehavior_
+
+namespace strategy
+{
+   class QuackBehavior
+   {
+      public:
+         virtual void quack() = 0;
+	 
+      protected:
+      
+      private:
+            	
+    };
+}
+#endif //_quackbehavior_
