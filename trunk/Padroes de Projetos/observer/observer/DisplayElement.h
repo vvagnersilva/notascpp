f25a5a71c00c88be7a207a9b201921da8e589530
vvagner.silva <vvagner.silva@923c45c9-312b-0410-8b6e-5d8b477a1069>
2007-03-09 16:09:53 +0000

    Padrão Observer

---
+++ trunk/Padroes de Projetos/observer/observer/DisplayElement.h
@@ -0,0 +1,14 @@
+#ifndef _displayelement_
+#define _displayelement_
+
+namespace observer
+{
+   class DisplayElement
+   {
+      public:
+         virtual void display();
+       
+      private:
+    };
+}
+#endif //_displayelement_
